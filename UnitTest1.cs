b22434c39f72c90bbee5c20389c2a2de11308caf
Adam Ateye <S01760@newday.co.uk>
2022-08-02 15:25:46 +0100

    changes

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,6 +14,29 @@ namespace DiamondKataTest
             Assert.Equal(expected.Count, actual.Count);
             Assert.Equal(expected[0], actual[0]);
         }
+        public void Test2()
+        {
+            // arrange
+            var input = 'B';
+            var expected = new List<string> {" A","B"};  
+            // act
+            var actual = MakeDiamond(input);
+            // assert
+            Assert.Equal(expected.Count, actual.Count);
+            Assert.Equal(expected[2], actual[2]);
+        }
+
+            public List<string> MakeDiamond(char input)
+        {
+            var firstLetter = (int)'A';
+            var lastLetter = (int)input;
+            var dif = lastLetter - firstLetter;
+
+            var resultList = new List<string>();
+            resultList.Add(input.ToString());
+
+            return resultList;
+        }
     }
 
 
