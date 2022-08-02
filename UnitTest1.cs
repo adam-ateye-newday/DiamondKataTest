6dd06edb51ffd68250114af23535300ae6563c75
Jamie Orr <Jamie.Orr@newday.co.uk>
2022-08-02 15:48:55 +0100

    top left diagonal of diamond

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -15,6 +15,32 @@ namespace DiamondKataTest
             Assert.Equal(expected[0], actual[0]);
         }
 
+        [Fact]
+        public void TwoLine()
+        {
+            // arrange
+            var input = 'B';
+            var expected = new List<string>() { " A", "B" };
+            // act
+            var actual = MakeDiamond(input);
+            // assert
+            Assert.Equal(expected.Count, actual.Count);
+            Assert.Equal(expected[0], actual[0]);
+        }
+
+        [Fact]
+        public void ThreeLine()
+        {
+            // arrange
+            var input = 'C';
+            var expected = new List<string>() { "  A", " B", "C" };
+            // act
+            var actual = MakeDiamond(input);
+            // assert
+            Assert.Equal(expected.Count, actual.Count);
+            Assert.Equal(expected[0], actual[0]);
+        }
+
         public List<string> MakeDiamond(char input)
         {
             var dif = (int)input - (int)'A';
