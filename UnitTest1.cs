b888f229d7575704c0ef46268006d4289fe29a41
Conor Kelly <conor.kelly@newday.co.uk>
2022-08-02 14:44:31 +0100

    create first unit test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,7 +5,14 @@ namespace DiamondKataTest
         [Fact]
         public void Test1()
         {
-
+            // arrange
+            var input = 'A';
+            var expected = new List<string>() { "A"};
+            // act
+            var actual = MakeDiamond(input);
+            // assert
+            Assert.Equal(expected.Count, actual.Count);
+            Assert.Equal(expected[0], actual[0]);
         }
     }
 }
