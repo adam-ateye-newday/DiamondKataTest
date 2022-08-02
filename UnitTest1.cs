63ee1b13f8dc3cf87bb500ec9f12000e95419d45
Jamie Orr <Jamie.Orr@newday.co.uk>
2022-08-02 15:03:34 +0100

    added MakeDiamond method

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,5 +14,13 @@ namespace DiamondKataTest
             Assert.Equal(expected.Count, actual.Count);
             Assert.Equal(expected[0], actual[0]);
         }
+
+        public List<string> MakeDiamond(char input)
+        {
+            var resultList = new List<string>();
+            resultList.Add(input.ToString());
+            
+            return resultList;
+        }
     }
 }
