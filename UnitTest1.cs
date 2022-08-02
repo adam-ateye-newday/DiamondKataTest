31fa63b8c2e0b700915d85ca227fba91a18917ee
Adam Ateye <S01760@newday.co.uk>
2022-08-02 17:38:18 +0100

    complete triangle

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -20,12 +20,13 @@ namespace DiamondKataTest
         {
             // arrange
             var input = 'B';
-            var expected = new List<string>() { " A", "B" };
+            var expected = new List<string>() { " A", "B B" };
             // act
             var actual = MakeDiamond(input);
             // assert
             Assert.Equal(expected.Count, actual.Count);
             Assert.Equal(expected[0], actual[0]);
+            Assert.Equal(expected[1], actual[1]);
         }
 
         [Fact]
@@ -33,25 +34,34 @@ namespace DiamondKataTest
         {
             // arrange
             var input = 'C';
-            var expected = new List<string>() { "  A", " B", "C" };
+            var expected = new List<string>() { "  A", " B B", "C   C" };
             // act
             var actual = MakeDiamond(input);
             // assert
             Assert.Equal(expected.Count, actual.Count);
             Assert.Equal(expected[0], actual[0]);
+            Assert.Equal(expected[1], actual[1]);
+            Assert.Equal(expected[2], actual[2]);
         }
 
         public List<string> MakeDiamond(char input)
         {
             var dif = (int)input - (int)'A';
-            var before = dif;
-            //int between = 0;
 
             var resultList = new List<string>();
 
             for (var i = 0; i <= dif; i++)
             {
-                var line = GenerateSpaces(dif - i) + (char)((int)'A' + i);
+                var currentLetter = (char)((int)'A' + i);
+
+                var between = currentLetter == 'A' ? 0 : i * 2 - 1;
+                
+                var line = GenerateSpaces(dif - i) + currentLetter + GenerateSpaces(between);
+                if(between > 0) 
+                {
+                    line += currentLetter;
+                }
+
                 resultList.Add(line);
             }
 
@@ -60,14 +70,8 @@ namespace DiamondKataTest
 
         public string GenerateSpaces(int spaces)
         {
-            var result = "";
-
-            while (result.Length < spaces)
-            {
-                result = result + " ";
-            } 
-
-            return result;
+            return new string(' ', spaces);
+      
         }
     }
 }
